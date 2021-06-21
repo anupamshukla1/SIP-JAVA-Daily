9c545e3bf2f55f69157161a7394f5bb5828a660a
anupamshukla1 <anupamshuklaankit@gmail.com>
2021-06-22 00:35:39 +0530

    day 1 basics

---
+++ src/ScannerInput.java
@@ -0,0 +1,16 @@
+//22 june 2021
+//Program to take user input
+//Basic-1
+
+
+import java.util.Scanner;
+
+public class ScannerInput {
+    public static void main(String []args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number");
+        int value1 = sc.nextInt();
+            System.out.println("The number you input is :" +value1);
+    }
+}
+
